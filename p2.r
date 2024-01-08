e7a4dbdc9698be95f4c468f086c623c8d87f6dd6
nisargashindhe <124601195+nisargashindhe@users.noreply.github.com>
2024-01-08 09:28:13 +0530

    p2.r

---
+++ p2.r
@@ -0,0 +1,21 @@
+2)Write a R program that include variables, constants, data types
+
+logic <- TRUE
+cat(logic,"\n")
+cat("the data type of variable logic is",class(logic), "\n\n")
+
+numeric <- 106
+cat(numeric,"\n")
+cat("the data type of variable numeric is",class(numeric), "\n\n")
+integer <- 133L
+cat(integer,"\n")
+cat("the data type of variable integer is",class(integer), "\n\n")
+complex <- 2+3i
+cat(complex,"\n")
+cat("the data type of variable complex is",class(complex), "\n\n")
+character <- "R Programming"
+cat(character,"\n")
+cat("the data type of variable character is",class(character), "\n\n")
+raw <- charToRaw("R Programming")
+cat(raw,"\n")
+cat("the data type of variable raw is",class(raw), "\n\n")
