52e64bbcb0159131750123a18d306a4abd38bebd
nisargashindhe <124601195+nisargashindhe@users.noreply.github.com>
2024-01-08 09:36:30 +0530

    Create p3.r

---
+++ p3.r
@@ -0,0 +1,8 @@
+3) Write a R program for calculating cumulative sums, and products minima maxima and
+calculus.
+
+val<-c(2,3,5,6,8,4,9,10)
+cat("cumulative Sum::", cumsum(val),"\n")
+cat("cumulative Product::", cumprod(val),"\n")
+cat("cumulative Minimum::", cummin(val),"\n")
+cat("cumulative Maximum::", cummax(val),"\n")
